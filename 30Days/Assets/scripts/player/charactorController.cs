3291e9c02fc59bc00e91688d087c905054da8f84
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2023-09-05 21:55:22 +0900

    dkl
    ddd

--- 30Days/Assets/scripts/player/charactorController.cs
+++ 30Days/Assets/scripts/player/charactorController.cs
@@ -14,18 +14,33 @@ public class charactorController : MonoBehaviour
     private string currentState;
     int dir;
 
+    [Header("Point")]
+    public Transform plane;
+
     string[] wk = new string[] { "walk_front", "walk_back", "walk_side" };
     string[] id = new string[] { "idle_ront", "idle_back", "idle_side" };
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void Update()
     {
         movement();
         set_direction();
+        put_plane();
+    }
+
+    public void put_plane()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            Vector3 p = snapToGrid(hit.point);
+            plane.position = p;
+            Debug.Log(p);
+        }
     }
 
     public void movement()
@@ -69,5 +84,22 @@ public class charactorController : MonoBehaviour
         ani.Play(newState);
 
         currentState = newState;
+    }\
+
+    float dis(Vector3 a, Vector3 b)
+    {
+        return Mathf.Sqrt(Mathf.)
+    }
+
+    Vector3 snapToGrid(Vector3 pos)
+    {
+        if (Mathf.Abs(Vector3.Distance(transform.position, pos)) <= 1)
+        {
+            return new Vector3(Mathf.Round(pos.x), Mathf.Round(pos.y), Mathf.Round(pos.z));
+        }
+        else
+        {
+            return Vector3.zero;
+        }
     }
 }
